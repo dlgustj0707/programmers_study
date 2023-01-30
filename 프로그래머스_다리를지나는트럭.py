ff6734b9ee08819ca47558352848711abd7806fc
이현서 <121599583+dlgustj0707@users.noreply.github.com>
2023-01-30 18:34:36 +0900

    Update 프로그래머스_다리를지나는트럭.py

--- 프로그래머스_다리를지나는트럭.py
+++ 프로그래머스_다리를지나는트럭.py
@@ -1,17 +1,14 @@
 def solution(bridge_length, weight, truck_weights):
-    second = 0
-    bridge_crossing = [0] * bridge_length
+    second = 0 #시간
+    bridge_crossing = [0] * bridge_length #다리를 건너는 트럭 리스트 
+    #다리의 길이 만큼 0을 넣은 리스트 생성
     while bridge_crossing != []: 
         second += 1
         bridge_crossing.pop(0)
         if len(truck_weights) != 0:
-            if sum(bridge_crossing) + truck_weights[0] < weight:
+            if sum(bridge_crossing) + truck_weights[0] <= weight:
                 bridge_crossing.append(truck_weights[0])
                 truck_weights.pop(0)
             else:
                 bridge_crossing.append(0)
-            print(bridge_crossing)
-            print(truck_weights)
     return second
-
-print(solution(2, 10, [7,4,5,6]))
